44e4f820e339ed56708855ba7bc5deb67f649ead
joye1503 <68348207+joye1503@users.noreply.github.com>
2020-08-06 16:33:03 +0530

    Update test_backends_interactive.py

--- lib/matplotlib/tests/test_backends_interactive.py
+++ lib/matplotlib/tests/test_backends_interactive.py
@@ -147,7 +147,7 @@ _test_timeout = 10  # Empirically, 1s is not enough on Travis.
 @pytest.mark.flaky(reruns=3)
 def test_interactive_backend(backend, toolbar):
     #output = subprocess.check_output(['uname', '-m'])
-    if platform.machine() == 'ARM64':
+    if platform.machine() == 'aarch64':
         pytest.skip("Skipping for aarch64 architecture")
     if backend == "macosx" and toolbar == "toolmanager":
         pytest.skip("toolmanager is not implemented for macosx.")
